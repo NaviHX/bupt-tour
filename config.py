c1430f6ae346fde34598fb75dba78db3d26bae64
navihx <932942142@qq.com>
2021-03-26 15:25:04 +0800

    添加负载均衡与逻辑地址配置项目

--- config.py
+++ config.py
@@ -9,22 +9,24 @@ broad=int(lines[0][2])
 v=int(lines[1][0])
 bv=int(lines[1][1])
 tt=int(lines[2])
-out.write("{0} {1} {2}\n{3} {4}\n{5}\n".format(total,road,broad,v,bv,tt))
+fz=int(lines[3])
+lj=int(lines[4])
+out.write("{0} {1} {2}\n{3} {4}\n{5}\n{6}\n{7}\n".format(total,road,broad,v,bv,tt,fz,lj))
 
 dic={}
-i=3
+i=5
 offset=0
-while i<3+total+offset:
+while i<5+total+offset:
     if lines[i][0]=="#":
         offset += 1
         i += 1
         continue
     out.write(lines[i])
     lines[i]=lines[i].split(" ")
-    dic[lines[i][0]]=i-3-offset
+    dic[lines[i][0]]=i-5-offset
     i += 1
 
-i=3+total+offset
+i=5+total+offset
 st=i
 offset=0
 while i<st+road+offset:
@@ -50,3 +52,32 @@ while i<st+broad+offset:
     lines[i][1]=dic[lines[i][1]]
     out.write("{0} {1} {2} {3} {4}".format(lines[i][0],lines[i][1],lines[i][2],lines[i][3],lines[i][4]))
     i += 1
+
+st=i
+offset=0
+while i<st+fz+offset:
+    if lines[i][0]=="#":
+        offset += 1
+        i += 1
+        continue
+    lines[i]=lines[i].split(" ")
+    out.write("{0} {1} ".format(lines[i][0],lines[i][1]))
+    j=2
+    while j<len(lines[i]):
+        lines[i][j]=dic[lines[i][j]]
+        out.write("{0} {1} ".format(lines[i][j],lines[i][j+1]))
+        j+=2
+    out.write("\n")
+    i+=1
+
+st=i
+offset=0
+while i<st+lj+offset:
+    if lines[i][0]=="#":
+        offset += 1
+        i += 1
+        continue
+    lines[i]=lines[i].split(" ")
+    lines[i][0]=dic[lines[i][0]]
+    out.write("{0} {1}\n".format(lines[i][0],lines[i][1]))
+    i+=1
